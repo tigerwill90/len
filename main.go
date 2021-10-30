c92255086cd4871c526d7750cf1b7854c1d3196f
sylvain <sylvain.muller90gmail.com>
2021-10-30 22:58:39 +0200

    Stdin support & format length with comma

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"bufio"
+	"errors"
 	"flag"
 	"fmt"
+	"github.com/dustin/go-humanize"
+	"io"
+	"os"
+	"strings"
 )
 
 func main() {
+	var s string
+	flag.StringVar(&s, "s", "", "input string")
 	flag.Parse()
-	s := flag.Arg(0)
-	fmt.Printf("%d\n", len([]rune(s)))
+
+	count := 0
+	var r io.Reader
+	if s != "" {
+		r = strings.NewReader(s)
+	} else {
+		count -= 1
+		r = bufio.NewReader(os.Stdin)
+	}
+
+	buf := make([]byte, 1*1024*1024)
+	for {
+		n, err := r.Read(buf)
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			panic(err)
+		}
+		count += len([]rune(string(buf[:n])))
+	}
+
+	fmt.Printf("%s\n", humanize.Comma(int64(count)))
 }
